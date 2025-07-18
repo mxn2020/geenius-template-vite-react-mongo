4cc64a6932fa93bedcdb1c7cf1143ad6158fb17b
Mehdi Nabhani <mehdi@nabhani.de>
2025-07-18 18:44:32 +0200

    vite fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.html'],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
